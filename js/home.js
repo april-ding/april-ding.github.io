3bd6d2175e96f1375522156cb8cd53b410f93725
April Ding <apr.ding@gmail.com>
2019-04-22 19:22:56 -0700

    IMPORTED AND FIXED EVERYTHING FROM V4
    i actually want to cry
    its all fixed up to when i started to try to merge to main

--- js/home.js
+++ js/home.js
@@ -1,61 +1,76 @@
+// $("#empty-three-d").one("mouseover", function() {
+//   $("#thumbnail-flex-3d").addClass('permahover');
+//   $("#thumbnail-flex-2-3d").addClass('permahover');
+// });
+// $("#empty-gd").one("mouseover", function() {
+//   $("#thumbnail-flex-gd").addClass('permahover');
+//   $("#thumbnail-flex-2-gd").addClass('permahover');
+// });
+// $("#empty-mg").one("mouseover", function() {
+//   $(".thumbnail-flex").addClass('permahover');
+//   $(".thumbnail-flex-2").addClass('permahover');
+// });
+
+
 //p5
 var canvas;
 let rectX = 0;
 let rectY = 0;
 let patternNum = 1;
-
+let sqSize = 10;
 
 function setup(){
     canvas = createCanvas(windowWidth, windowHeight);
 
 }
 function timeIt(){
-    rectX = int(30*random(0, windowWidth/30));
-    rectY = int(30*random(0, windowHeight/30));
+    rectX = int(sqSize*random(0, windowWidth/sqSize));
+    rectY = int(sqSize*random(0, windowHeight/sqSize));
 }
 
 function pattern1(){
-    rect(rectX, rectY, 30, 30);
-    rect(rectX + 30, rectY, 30, 30);
-    rect(rectX + 30, rectY + 30, 30, 30);
+    rect(rectX, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY + sqSize, sqSize, sqSize);
 }
 function pattern2(){
-    rect(rectX, rectY, 30, 30);
-    rect(rectX + 60, rectY, 30, 30);
+     rect(rectX, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY, sqSize, sqSize);
 
 }
 function pattern3(){
-    rect(rectX, rectY, 30, 30);
-    rect(rectX + 30, rectY + 30, 30, 30);
-    rect(rectX + 60, rectY + 60, 30, 30);
+   rect(rectX, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY + sqSize, sqSize, sqSize);
+    rect(rectX + sqSize*2, rectY + sqSize*2, sqSize, sqSize);
 }
 function pattern4(){
-    rect(rectX, rectY, 30, 30);
-    rect(rectX, rectY + 30, 30, 30);
-    rect(rectX, rectY + 30, 60, 30);
-    rect(rectX, rectY + 30, 90, 30);
+    rect(rectX, rectY, sqSize, sqSize);
+    rect(rectX, rectY + sqSize, sqSize, sqSize);
+    rect(rectX, rectY + sqSize, sqSize, sqSize);
+    rect(rectX, rectY + sqSize, sqSize, sqSize);
 }
 function pattern5(){
-    rect(rectX, rectY, 30, 30);
-    rect(rectX + 30, rectY, 30, 30);
-    rect(rectX + 30, rectY + 30, 60, 30);
-    rect(rectX, rectY + 30, 90, 30);
+    rect(rectX, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY, sqSize, sqSize);
+    rect(rectX + sqSize, rectY + sqSize, sqSize*2, sqSize);
+    rect(rectX, rectY + sqSize, sqSize*3, sqSize);
 }
 
 function draw() {
     stroke(255, 216, 230);
     strokeWeight(2);
     //draw grid
-    for (var x = 30; x <= windowWidth; x += 30){
-        line(x, 0, x, windowHeight);
-    }
-    for (var y = 30; y <= windowHeight; y += 30){
-        line(0, y, windowWidth, y);
-    }
+    // for (var x = 30; x <= windowWidth; x += 30){
+    //     line(x, 0, x, windowHeight);
+    // }
+    // for (var y = 30; y <= windowHeight; y += 30){
+    //     line(0, y, windowWidth, y);
+    // }
     noStroke();
-    fill(255, 216, 230);
+     // fill(255, 216, 230);
+    fill('cornFlowerBlue');
 
-    if (frameCount % 60 == 0) {
+    if (frameCount % 30 == 0) {
         timeIt();
 
         patternNum = int(random(1,6));
