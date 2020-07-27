58522ff1631c33a709247b87b5f2b7929f90d81f
April Ding <apr.ding@gmail.com>
2020-07-26 22:24:06 -0700

    disable stats

--- js/home.js
+++ js/home.js
@@ -3,17 +3,21 @@ new WOW().init();
 //************ AOS ****************//
 AOS.init();
 /********* STATS ************/
+var disableStats = true;
 
 javascript: (function() {
-    var script = document.createElement('script');
-    script.onload = function() {
-        var stats = new Stats();
-        document.body.appendChild(stats.dom);
-        requestAnimationFrame(function loop() {
-            stats.update();
-            requestAnimationFrame(loop)
-        });
-    };
-    script.src = 'js/stats.min.js';
-    document.head.appendChild(script);
+    if(!disableStats){
+        var script = document.createElement('script');
+        script.onload = function() {
+            var stats = new Stats();
+            document.body.appendChild(stats.dom);
+            requestAnimationFrame(function loop() {
+                stats.update();
+                requestAnimationFrame(loop)
+            });
+        };
+        script.src = 'js/stats.min.js';
+        document.head.appendChild(script);
+    }
+
 })()
--- js/standard-project.js
+++ js/standard-project.js
@@ -1,14 +1,19 @@
 /********* STATS ************/
+var disableStats = true;
+
 javascript: (function() {
-    var script = document.createElement('script');
-    script.onload = function() {
-        var stats = new Stats();
-        document.body.appendChild(stats.dom);
-        requestAnimationFrame(function loop() {
-            stats.update();
-            requestAnimationFrame(loop)
-        });
-    };
-    script.src = '../js/stats.min.js';
-    document.head.appendChild(script);
+    if(!disableStats){
+        var script = document.createElement('script');
+        script.onload = function() {
+            var stats = new Stats();
+            document.body.appendChild(stats.dom);
+            requestAnimationFrame(function loop() {
+                stats.update();
+                requestAnimationFrame(loop)
+            });
+        };
+        script.src = 'js/stats.min.js';
+        document.head.appendChild(script);
+    }
+
 })()
