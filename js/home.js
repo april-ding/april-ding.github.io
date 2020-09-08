65166470c3361faa6aca3cc31c7535232ecb2756
April Ding <apr.ding@gmail.com>
2020-09-07 21:56:02 -0700

    mobile hamburger menu dropdown working

--- js/home.js
+++ js/home.js
@@ -6,7 +6,7 @@ AOS.init();
 var disableStats = true;
 
 javascript: (function() {
-    if(!disableStats){
+    if (!disableStats) {
         var script = document.createElement('script');
         script.onload = function() {
             var stats = new Stats();
@@ -21,3 +21,34 @@ javascript: (function() {
     }
 
 })()
+/********* NAV ************/
+$(document).ready(function() {
+    $('.icon').on("click", function() {
+        openNav();
+    })
+});
+
+function showWork() {
+    var y = document.getElementById("dropdown-content");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+function openNav() {
+    var x = $("#nav-mobile-links");
+
+    if (x.css('display') == 'block') { //nav opened, need to close
+        $("body").css("overflow", "visible");
+        x.removeClass("nav-open").addClass("nav-close");
+        setTimeout(function() {
+            x.css('display', 'none');
+        }, 200);
+    } else { //nav closed, need to open
+        $("body").css("overflow", "hidden");
+        x.removeClass("nav-close").addClass("nav-open");
+        x.css("display", "block");
+    }
+}
