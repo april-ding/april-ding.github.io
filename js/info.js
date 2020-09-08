b92847ae36dfbe84b813dd2264de141ab8e3888e
April Ding <apr.ding@gmail.com>
2020-09-08 02:29:59 -0700

    apply new nav(dropdown) to all pages

--- js/info.js
+++ js/info.js
@@ -1,29 +1,3 @@
-
-// document.getElementsByClassName('vid').play();
-
-
-AOS.init();
-
-/********* STATS ************/
-var disableStats = true;
-
-javascript: (function() {
-    if(!disableStats){
-        var script = document.createElement('script');
-        script.onload = function() {
-            var stats = new Stats();
-            document.body.appendChild(stats.dom);
-            requestAnimationFrame(function loop() {
-                stats.update();
-                requestAnimationFrame(loop)
-            });
-        };
-        script.src = 'js/stats.min.js';
-        document.head.appendChild(script);
-    }
-
-})()
-
 /********* NAV ************/
 $(document).ready(function() {
     $('.icon').on("click", function() {
